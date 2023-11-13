f0c83bbd7744e095f33dbaac0859c14e780c7310
Skunkkzen <a18586@alunos.ipca.pt>
2023-11-13 12:56:27 +0000

    Correções classe segurança, inserção de nova classe section que irá definir a secção do segurança.

---
+++ Entities/Section.cs
@@ -0,0 +1,18 @@
+﻿namespace Safari_Management.Entities;
+
+[Serializable] // Marcar a classe como serializável
+class Section
+{
+    public string Name { get; set; }
+
+    public Section() { }
+    public Section(string name)
+    {
+        Name = name;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
